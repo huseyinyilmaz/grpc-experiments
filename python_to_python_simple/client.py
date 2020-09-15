cfff4717d7a08813c750aa47ae5e452cb261f980
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2020-09-14 20:45:50 -0700

    Integrate black.

--- python_to_python_simple/client.py
+++ python_to_python_simple/client.py
@@ -1,22 +1,27 @@
 import logging
-
 import grpc
 
 import python_to_python_simple_pb2
 import python_to_python_simple_pb2_grpc
 from datetime import datetime
 
+
 REQUEST_COUNT = 10000
 
+
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel(
+        "localhost:50051", compression=grpc.Compression.Gzip
+    ) as channel:
+
         stub = python_to_python_simple_pb2_grpc.Simple_python_pythonStub(channel)
         start = datetime.now()
         for i in range(REQUEST_COUNT):
             response = stub.increase(python_to_python_simple_pb2.IncreaseRequest(num=i))
-            assert response.num == i+1
-        print(f'{REQUEST_COUNT} requests is completed in {datetime.now() - start}')
+            assert response.num == i + 1
+        print(f"{REQUEST_COUNT} requests is completed in {datetime.now() - start}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig()
     run()
